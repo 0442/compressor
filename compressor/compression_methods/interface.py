7cf8eac28a020d25a5a8c869036c1b67aac61c0f
0442 <82746@proton.me>
2024-11-16 22:21:13 +0200

    Update argument names

--- compressor/compression_methods/interface.py
+++ compressor/compression_methods/interface.py
@@ -2,27 +2,25 @@ from abc import ABC, abstractmethod
 from typing import TextIO, BinaryIO
 
 class CompressionMethod(ABC):
-  """ABC defining an interface for different text compression methods."""
-  @abstractmethod
-  def compress(self, input: TextIO, otuput: BinaryIO) -> None:
-    """
-    Parameters
-    ----------
-    input : TextIO
-        TextIO object from which compressable text data is read.
-    output : BinaryIO 
-        BinaryIO object to which compressed output is written to.
-    """
-    ...
+    """ABC defining an interface for different text compression methods."""
+    @abstractmethod
+    def compress(self, text_in: TextIO, bin_out: BinaryIO) -> None:
+        """
+        Parameters
+        ----------
+        input : TextIO
+            TextIO object from which compressable text data is read.
+        output : BinaryIO
+            BinaryIO object to which compressed output is written to.
+        """
 
-  @abstractmethod
-  def uncompress(self, input: BinaryIO, output: TextIO) -> None:
-    """
-    Parameters
-    ----------
-    input : BinaryIO
-        BinaryIO object from which compressed data is read.
-    output : TextIO
-        TextIO object to which uncompressed text output is written to.
-    """
-    ...
+    @abstractmethod
+    def uncompress(self, bin_in: BinaryIO, text_out: TextIO) -> None:
+        """
+        Parameters
+        ----------
+        input : BinaryIO
+            BinaryIO object from which compressed data is read.
+        output : TextIO
+            TextIO object to which uncompressed text output is written to.
+        """
