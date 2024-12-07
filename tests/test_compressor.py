1ec3048a38eeb75c5e5bf5f6ac588a3499ebf8fc
0442 <82746@proton.me>
2024-12-07 19:38:02 +0200

    Add tests for `Compressor`

---
+++ tests/test_compressor.py
@@ -0,0 +1,35 @@
+from pathlib import Path
+import filecmp
+
+from compressor.compressor import Compressor
+from compressor.compression_methods import LZW, Huffman
+
+from .common import LONG_TEXT_FILE
+
+
+def test_compressor_roundtrip_lzw(tmp_path: Path):
+    method = LZW()
+
+    tmp_compressed = tmp_path / "compressed.lzw"
+    tmp_decompressed = tmp_path / "decompressed.txt"
+
+    c = Compressor()
+    c.compress(str(LONG_TEXT_FILE), str(tmp_compressed), method)
+
+    c.decompress(str(tmp_compressed), str(tmp_decompressed), method)
+
+    assert filecmp.cmp(LONG_TEXT_FILE, tmp_decompressed)
+
+
+def test_compressor_roundtrip_huffman(tmp_path: Path):
+    method = Huffman()
+
+    tmp_compressed = tmp_path / "compressed.huffman"
+    tmp_decompressed = tmp_path / "decompressed.txt"
+
+    c = Compressor()
+    c.compress(str(LONG_TEXT_FILE), str(tmp_compressed), method)
+
+    c.decompress(str(tmp_compressed), str(tmp_decompressed), method)
+
+    assert filecmp.cmp(LONG_TEXT_FILE, tmp_decompressed)
